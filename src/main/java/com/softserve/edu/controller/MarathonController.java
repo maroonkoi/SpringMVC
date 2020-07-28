2a23e638cd0f2bba7ae4f613e003e8f4319ca105
nkvICEHW <slotvinskiy@gmail.com>
2020-07-28 14:10:45 +0300

    added marathon.html

--- src/main/java/com/softserve/edu/controller/MarathonController.java
+++ src/main/java/com/softserve/edu/controller/MarathonController.java
@@ -2,19 +2,21 @@ package com.softserve.edu.controller;
 
 
 import com.softserve.edu.service.MarathonService;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@AllArgsConstructor
 @Data
 public class MarathonController {
     private MarathonService marathonService;
 
     @GetMapping("/marathons")
     public String getAllMarathons(Model model) {
-        //TODO
+        model.addAttribute("marathons", marathonService.getAll());
         return "marathons";
     }
 

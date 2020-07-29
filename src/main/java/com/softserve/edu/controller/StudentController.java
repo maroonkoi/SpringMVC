41d18b87f7575e6f5391b2d0eded3f7eec5cce73
Andrii <simanoav@gmail.com>
2020-07-29 11:39:14 +0300

    added getAllUsers and getAllUsersByMarathon methods

--- src/main/java/com/softserve/edu/controller/StudentController.java
+++ src/main/java/com/softserve/edu/controller/StudentController.java
@@ -1,15 +1,39 @@
 package com.softserve.edu.controller;
 
+import com.softserve.edu.model.Marathon;
+import com.softserve.edu.model.User;
+import com.softserve.edu.service.MarathonService;
 import com.softserve.edu.service.UserService;
 import lombok.Data;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+import java.util.Set;
 
 
 @Controller
 @Data
 public class StudentController {
 
-    private UserService userService;
+    private final UserService userService;
+    private final MarathonService marathonService;
+
+    @GetMapping("/users")
+    public String viewAllUsers(Model model) {
+        model.addAttribute("users", userService.getAll());
+        return "users";
+    }
+
+    @GetMapping("/users/{id}")
+    public String viewAllUsersByMarathon (@PathVariable String id, Model model){
+        Marathon marathon = marathonService.getMarathonById(Long.valueOf(id));
+        Set<User> users = marathon.getUsers();
+        model.addAttribute("users", users);
+        return "users";
+    }
 
     //TODO implement needed methods
 

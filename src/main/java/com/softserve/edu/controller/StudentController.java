b4320c65f54b9b0f045fc8c2f6bd98ef87c39deb
Andrii <simanoav@gmail.com>
2020-07-29 23:29:44 +0300

    Update StudentController.java

--- src/main/java/com/softserve/edu/controller/StudentController.java
+++ src/main/java/com/softserve/edu/controller/StudentController.java
@@ -31,7 +31,7 @@ public class StudentController {
 
 
     @GetMapping("/students_marathon/{id}")
-    public String viewAllUsersByMarathon (@PathVariable Long id, Model model){
+    public String viewAllUsersByMarathon(@PathVariable Long id, Model model) {
         Marathon marathon = marathonService.getMarathonById(id);
         Set<User> users = marathon.getUsers();
         for (User user : users) {
@@ -55,38 +55,53 @@ public class StudentController {
     }
 
     @GetMapping("/student/{studentId}")
-    public String showStudent (@PathVariable Long studentId, Model model){
+    public String showStudent(@PathVariable Long studentId, Model model) {
         User user = userService.getUserById(studentId);
         model.addAttribute("student", user);
         return "student";
     }
+
     @GetMapping("/students/{marathonId}/add")
-    public String addStudentToMarathon (@PathVariable Long marathonId, Model model){
+    public String addStudentToMarathon(@PathVariable Long marathonId, Model model) {
         model.addAttribute("student", new User()).addAttribute("marathon", marathonService.getMarathonById(marathonId));
         return "create_student";
     }
 
     @PostMapping("/students/{marathonId}/add")
-    public String addStudentToMarathon (@ModelAttribute(name="student") User user, @PathVariable("marathonId") Long id) {
+    public String addStudentToMarathon(@ModelAttribute(name = "student") User user, @PathVariable("marathonId") Long id) {
         userService.addUserToMarathon(user, marathonService.getMarathonById(id));
-        return "redirect:/students_marathon/"+id.intValue();
+        return "redirect:/students_marathon/" + id.intValue();
     }
 
     @GetMapping("/students/{marathonId}/edit/{studentId}")
-    public String editStudent (@PathVariable("marathonId") Long marathonId, @PathVariable("studentId") Long studentId, Model model) {
+    public String editStudent(@PathVariable("marathonId") Long marathonId, @PathVariable("studentId") Long studentId, Model model) {
         Marathon marathon = marathonService.getMarathonById(marathonId);
         User user = userService.getUserById(studentId);
         model.addAttribute("student", user).addAttribute("marathon", marathon);
         return "edit_student";
     }
 
-    @PostMapping("/students/{marathonId}/edit/{studentId}")
-    public String editStudent (@ModelAttribute(name = "student") User user) {
-        userService.createOrUpdateUser(user);
-        return "redirect:/students";
+    @PostMapping("/students/{marathonId}/edit/{id}")
+    public String editStudent(@ModelAttribute("student") User user, Model model) {
+        user.setRole(User.Role.TRAINEE);
+        user.setPassword(userService.getUserById(user.getId()).getPassword());
+        User updatedUser = null;
+        try {
+            updatedUser = userService.createOrUpdateUser(user);
+        } catch (Exception e) {
+            e.getStackTrace();
+        }
+        if (updatedUser != null) {
+            model.addAttribute("user", user);
+            return "student";
+        } else {
+            model.addAttribute("user", user);
+            model.addAttribute("error", "You should fill all fields correctly!");
+            return "edit_student";
+        }
     }
 
-    private List<User> getAllStudents (List<User> users) {
+    private List<User> getAllStudents(List<User> users) {
         List<User> students = new ArrayList<>();
         for (User user : users) {
             if (user.getRole().equals(User.Role.TRAINEE)) {
@@ -98,9 +113,6 @@ public class StudentController {
     }
 
     //TODO implement needed methods
-
-
-
 
 
 }
